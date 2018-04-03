0db5e8cd287ca80de329017a151246928b325171
Ewerton Willams <ewertonews@hotmail.com>
2018-04-03 13:32:12 -0300

    Adição do projeto web-api

---
+++ Oradb2web-api/Controllers/QueryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Oradb2web_api.Controllers
+{
+   
+    public class QueryController : ApiController
+    {
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody]String query)
+        {
+            try
+            {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Deu certo");
+                
+            }
+            catch (Exception ex)
+            {
+
+                HttpError err = new HttpError(ex.Message);
+                return Request.CreateResponse(HttpStatusCode.NotFound, err);
+            }
+
+        }
+    }
+}
---
+++ Oradb2web-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oradb2web_api.WebApiApplication" Language="C#" %>
